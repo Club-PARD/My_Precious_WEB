701988faec6cb43424212afe7490f98047ed51bd
minzziPark <minzzi0068@gmail.com>
2023-12-20 21:44:56 +0900

    파이어베이스 크리덴셜 숨김

--- my_precious_project/src/API/googleLogin.js
+++ my_precious_project/src/API/googleLogin.js
@@ -2,12 +2,12 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA7_WIFJ2n7A2VWXPkSvCz10X-s8TUcRjk",
-  authDomain: "precious-relationship.firebaseapp.com",
-  projectId: "precious-relationship",
-  storageBucket: "precious-relationship.appspot.com",
-  messagingSenderId: "942898473483",
-  appId: "1:942898473483:web:23e2d775004447bdbd350e",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 const app = firebase.initializeApp(firebaseConfig);
