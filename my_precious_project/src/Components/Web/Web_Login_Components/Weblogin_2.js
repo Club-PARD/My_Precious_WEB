40f8400b7aecf46f98e3586b3d93d7ba09c1bfe7
minzziPark <minzzi0068@gmail.com>
2023-12-25 20:14:28 +0900

    충돌해결

--- my_precious_project/src/Components/Web/Web_Login_Components/Weblogin_2.js
+++ my_precious_project/src/Components/Web/Web_Login_Components/Weblogin_2.js
@@ -1,189 +1,154 @@
-<<<<<<< HEAD
 import React from "react";
 import styled, { ThemeProvider } from "styled-components";
-import { Link } from "react-router-dom";
-import { useTheme } from "../../../contexts/ThemeContext.js.js"; // Context APi 적용
-
-// theme 파일 폰트 적용 방법 + style-components 사용
-const Header1 = styled.div`
-  font-size: ${(props) => props.theme.Web_fontSizes.Header1};
-  font-weight: ${(props) => props.theme.fontWeights.Header1};
-  line-height: ${(props) => props.theme.LineHeight.Header1};
-  color: ${(props) => props.theme.colors.text};
-  font-family: "Pretendard";
-`;
-
-const Header2 = styled.div`
-  font-size: ${(props) => props.theme.Web_fontSizes.Header2};
-  font-weight: ${(props) => props.theme.fontWeights.Header2};
-  line-height: ${(props) => props.theme.LineHeight.Header2};
-  color: ${(props) => props.theme.colors.accent};
-  font-family: "Pretendard";
-`;
+import { useTheme } from "../../../contexts/ThemeContext.js"; // Context APi 적용
+import DotButton from "./DotButton.js";
 
 const WebLogin_2 = () => {
   const theme = useTheme();
-
   return (
     <ThemeProvider theme={theme}>
-      <div></div>
+      <Container>
+        <ContentBox>
+          <InnerRow1>
+            <DotButton dotColor={2} />
+          </InnerRow1>
+          <InnerRow2>
+            휴대폰 인증을 하면<br></br>전자서명이 가능해요!
+          </InnerRow2>
+          <InnerRow3>
+            전화번호<br></br>
+            <form>
+              <Label1>
+                <input type="text" placeholder="010-xxxx-xxxx"></input>
+                <button>인증번호 받기</button>
+              </Label1>
+              <Label2>
+                <input type="text" placeholder="인증번호 입력"></input>
+                <button>확인</button>
+              </Label2>
+            </form>
+          </InnerRow3>
+        </ContentBox>
+      </Container>
     </ThemeProvider>
   );
 };
 
-=======
-import React from 'react';
-import styled, { ThemeProvider } from 'styled-components';
-import { useTheme } from '../../../contexts/ThemeContext.js'; // Context APi 적용
-import DotButton from './DotButton.js';
-
-const WebLogin_2 = () => {
-    const theme = useTheme();
-    return (
-        <ThemeProvider theme={theme}>
-            <Container>
-                <ContentBox>
-                    <InnerRow1>
-                        <DotButton dotColor={2} />
-                    </InnerRow1>
-                    <InnerRow2>
-                        휴대폰 인증을 하면<br></br>전자서명이 가능해요!
-                    </InnerRow2>
-                    <InnerRow3>
-                        전화번호<br></br>
-                        <form>
-                            <Label1>
-                                <input type="text" placeholder="010-xxxx-xxxx"></input>
-                                <button>인증번호 받기</button>
-                            </Label1>
-                            <Label2>
-                                <input type="text" placeholder="인증번호 입력"></input>
-                                <button>확인</button>
-                            </Label2>
-                        </form>
-                    </InnerRow3>
-                </ContentBox>
-            </Container>
-        </ThemeProvider>
-    );
-};
-
 const Container = styled.div`
-    display: flex;
-    flex-direction: row;
-    margin: 0;
-    padding: 0;
-    height: 100vh;
-    justify-content: center;
+  display: flex;
+  flex-direction: row;
+  margin: 0;
+  padding: 0;
+  height: 100vh;
+  justify-content: center;
 `;
 
 const ContentBox = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    width: 400px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 400px;
 `;
 const InnerRow1 = styled.div`
-    display: flex;
-    justify-content: center;
-    width: 100%;
-    margin-top: 155px;
-    margin-bottom: 50px;
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  margin-top: 155px;
+  margin-bottom: 50px;
 `;
 
 const InnerRow2 = styled.div`
-    color: #000;
-    text-align: center;
-    font-size: 32px;
-    font-weight: 600;
-    line-height: 50px;
-    width: 100%;
-    margin-bottom: 100px;
+  color: #000;
+  text-align: center;
+  font-size: 32px;
+  font-weight: 600;
+  line-height: 50px;
+  width: 100%;
+  margin-bottom: 100px;
 `;
 
 const InnerRow3 = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    width: 100%;
-    font-size: 20px;
-    font-weight: 500;
-    color: #d9d9d9;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  font-size: 20px;
+  font-weight: 500;
+  color: #d9d9d9;
 `;
 
 const Label1 = styled.label`
-    position: relative;
-    width: 100%;
-    input {
-        margin-top: 30px;
-        width: 400px;
-        height: 55px;
-        border: 1.5px solid #ff3d00;
-        border-radius: 6.5px;
-        text-indent: 55px;
-        font-size: 20px;
-        font-weight: 500;
-        &::placeholder {
-            color: #d9d9d9;
-            font-size: 20px;
-            font-weight: 500;
-        }
+  position: relative;
+  width: 100%;
+  input {
+    margin-top: 30px;
+    width: 400px;
+    height: 55px;
+    border: 1.5px solid #ff3d00;
+    border-radius: 6.5px;
+    text-indent: 55px;
+    font-size: 20px;
+    font-weight: 500;
+    &::placeholder {
+      color: #d9d9d9;
+      font-size: 20px;
+      font-weight: 500;
     }
-    input:focus {
-        outline: none;
-    }
-    button {
-        position: absolute;
-        margin-top: 31px;
-        right: 0;
-        width: 150px;
-        height: 58px;
-        border-radius: 6.5px;
-        border: none;
-        color: white;
-        font-size: 20px;
-        font-weight: 500;
-        background-color: #ff3d00;
-        cursor: pointer;
-    }
+  }
+  input:focus {
+    outline: none;
+  }
+  button {
+    position: absolute;
+    margin-top: 31px;
+    right: 0;
+    width: 150px;
+    height: 58px;
+    border-radius: 6.5px;
+    border: none;
+    color: white;
+    font-size: 20px;
+    font-weight: 500;
+    background-color: #ff3d00;
+    cursor: pointer;
+  }
 `;
 
 const Label2 = styled.label`
-    position: relative;
-    width: 100%;
-    input {
-        margin-top: 30px;
-        width: 400px;
-        height: 55px;
-        border: 1.5px solid #ff3d00;
-        border-radius: 6.5px;
-        text-indent: 55px;
-        font-size: 20px;
-        font-weight: 500;
-        &::placeholder {
-            color: #d9d9d9;
-            font-size: 20px;
-            font-weight: 500;
-        }
+  position: relative;
+  width: 100%;
+  input {
+    margin-top: 30px;
+    width: 400px;
+    height: 55px;
+    border: 1.5px solid #ff3d00;
+    border-radius: 6.5px;
+    text-indent: 55px;
+    font-size: 20px;
+    font-weight: 500;
+    &::placeholder {
+      color: #d9d9d9;
+      font-size: 20px;
+      font-weight: 500;
     }
-    input:focus {
-        outline: none;
-    }
-    button {
-        position: absolute;
-        margin-top: 31px;
-        right: 0;
-        width: 150px;
-        height: 58px;
-        border-radius: 6.5px;
-        border: none;
-        color: white;
-        font-size: 20px;
-        font-weight: 500;
-        background-color: #ff3d00;
-        cursor: pointer;
-    }
+  }
+  input:focus {
+    outline: none;
+  }
+  button {
+    position: absolute;
+    margin-top: 31px;
+    right: 0;
+    width: 150px;
+    height: 58px;
+    border-radius: 6.5px;
+    border: none;
+    color: white;
+    font-size: 20px;
+    font-weight: 500;
+    background-color: #ff3d00;
+    cursor: pointer;
+  }
 `;
 
->>>>>>> main
 export default WebLogin_2;
--- my_precious_project/src/Components/Web/Web_Login_Components/Weblogin_3.js
+++ my_precious_project/src/Components/Web/Web_Login_Components/Weblogin_3.js
@@ -1,128 +1,94 @@
-<<<<<<< HEAD
 import React from "react";
 import styled, { ThemeProvider } from "styled-components";
-import { Link } from "react-router-dom";
-import { useTheme } from "../../../contexts/ThemeContext.js.js"; // Context APi 적용
-
-// theme 파일 폰트 적용 방법 + style-components 사용
-const Header1 = styled.div`
-  font-size: ${(props) => props.theme.Web_fontSizes.Header1};
-  font-weight: ${(props) => props.theme.fontWeights.Header1};
-  line-height: ${(props) => props.theme.LineHeight.Header1};
-  color: ${(props) => props.theme.colors.text};
-  font-family: "Pretendard";
-`;
-
-const Header2 = styled.div`
-  font-size: ${(props) => props.theme.Web_fontSizes.Header2};
-  font-weight: ${(props) => props.theme.fontWeights.Header2};
-  line-height: ${(props) => props.theme.LineHeight.Header2};
-  color: ${(props) => props.theme.colors.accent};
-  font-family: "Pretendard";
-`;
+import { useTheme } from "../../../contexts/ThemeContext.js"; // Context APi 적용
+import DotButton from "./DotButton.js";
+import { Checkmark } from "react-checkmark";
 
 const WebLogin_3 = () => {
   const theme = useTheme();
-
   return (
     <ThemeProvider theme={theme}>
-      <div></div>
+      <Container>
+        <ContentBox>
+          <InnerRow1>
+            <DotButton dotColor={3} />
+          </InnerRow1>
+          <InnerRow2>휴대폰 인증이 완료되었어요!</InnerRow2>
+          <InnerRow3>
+            <Checkmark size="xLarge" color="#ff3d00"></Checkmark>
+            <Div>
+              등록하신 휴대폰 인증을 통해<br></br>전자서명을 따로 하시지 않아도
+              되는 편리함이 생겼어요.
+            </Div>
+            <form>
+              <Button>확인</Button>
+            </form>
+          </InnerRow3>
+        </ContentBox>
+      </Container>
     </ThemeProvider>
   );
 };
 
-=======
-import React from 'react';
-import styled, { ThemeProvider } from 'styled-components';
-import { useTheme } from '../../../contexts/ThemeContext.js'; // Context APi 적용
-import DotButton from './DotButton.js';
-import { Checkmark } from 'react-checkmark';
-
-const WebLogin_3 = () => {
-    const theme = useTheme();
-    return (
-        <ThemeProvider theme={theme}>
-            <Container>
-                <ContentBox>
-                    <InnerRow1>
-                        <DotButton dotColor={3} />
-                    </InnerRow1>
-                    <InnerRow2>휴대폰 인증이 완료되었어요!</InnerRow2>
-                    <InnerRow3>
-                        <Checkmark size="xLarge" color="#ff3d00"></Checkmark>
-                        <Div>
-                            등록하신 휴대폰 인증을 통해<br></br>전자서명을 따로 하시지 않아도 되는 편리함이 생겼어요.
-                        </Div>
-                        <form>
-                            <Button>확인</Button>
-                        </form>
-                    </InnerRow3>
-                </ContentBox>
-            </Container>
-        </ThemeProvider>
-    );
-};
-
 const Container = styled.div`
-    display: flex;
-    flex-direction: row;
-    margin: 0;
-    padding: 0;
-    height: 100vh;
-    justify-content: center;
+  display: flex;
+  flex-direction: row;
+  margin: 0;
+  padding: 0;
+  height: 100vh;
+  justify-content: center;
 `;
 
 const ContentBox = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    width: 500px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 500px;
 `;
 const InnerRow1 = styled.div`
-    display: flex;
-    justify-content: center;
-    width: 100%;
-    margin-top: 155px;
-    margin-bottom: 50px;
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  margin-top: 155px;
+  margin-bottom: 50px;
 `;
 
 const InnerRow2 = styled.div`
-    color: #000;
-    text-align: center;
-    font-size: 32px;
-    font-weight: 600;
-    line-height: 50px;
-    width: 100%;
-    margin-bottom: 100px;
+  color: #000;
+  text-align: center;
+  font-size: 32px;
+  font-weight: 600;
+  line-height: 50px;
+  width: 100%;
+  margin-bottom: 100px;
 `;
 
 const InnerRow3 = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    text-align: center;
-    width: 100%;
-    font-size: 20px;
-    font-weight: 500;
-    color: #d9d9d9;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  width: 100%;
+  font-size: 20px;
+  font-weight: 500;
+  color: #d9d9d9;
 `;
 
 const Div = styled.div`
-    margin-top: 50px;
+  margin-top: 50px;
 `;
 
 const Button = styled.button`
-    margin-top: 100px;
-    width: 450px;
-    height: 55px;
-    border-radius: 6.5px;
-    border: none;
-    color: white;
-    font-size: 20px;
-    font-weight: 500;
-    background-color: #ff3d00;
-    cursor: pointer;
+  margin-top: 100px;
+  width: 450px;
+  height: 55px;
+  border-radius: 6.5px;
+  border: none;
+  color: white;
+  font-size: 20px;
+  font-weight: 500;
+  background-color: #ff3d00;
+  cursor: pointer;
 `;
 
->>>>>>> main
 export default WebLogin_3;
