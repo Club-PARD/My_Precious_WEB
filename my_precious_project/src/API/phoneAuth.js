db5625daa43e8465972ed2b81ec110566d1ca125
minzziPark <86656147+minzziPark@users.noreply.github.com>
2023-12-26 01:16:39 +0900

    Mj branch (#18)
    * 파일 구조 변경
    
    * 병합 해결
    
    * 충돌 해결
    
    * 충돌해결
    
    * 변경사항 적용
    
    * 휴대폰 로그인 코드 삽입

---
+++ my_precious_project/src/API/phoneAuth.js
@@ -0,0 +1,50 @@
+import { RecaptchaVerifier, signInWithPhoneNumber } from "firebase/auth";
+import { auth } from "./firebaseAPI";
+
+function getPhoneNumberFromUserInput(phoneNumber) {
+  const regex = /[^0-9]/g;
+  const numericOnly = phoneNumber.replace(regex, "");
+  // console.log(numericOnly);
+
+  if (numericOnly.startsWith("010") && numericOnly.length === 11) {
+    console.log("+82" + numericOnly.slice(1));
+    return "+82" + numericOnly.slice(1);
+  }
+}
+
+export function handlePhoneButtonClick(number) {
+  window.recaptchaVerifier = new RecaptchaVerifier(
+    auth,
+    "recaptcha-container",
+    {
+      size: "invisible",
+      callback: (response) => {},
+    }
+  );
+  auth.languageCode = "ko"; // 한국어로 해줍시다
+  const phoneNumber = getPhoneNumberFromUserInput(number); // 위에서 받아온 번호
+  const appVerifier = window.recaptchaVerifier;
+  signInWithPhoneNumber(auth, phoneNumber, appVerifier)
+    .then((confirmationResult) => {
+      window.confirmationResult = confirmationResult; // window
+    })
+    .catch((error) => {
+      console.log("SMS FAILED");
+    });
+}
+
+export function handleAuthButtonClick(authNumber) {
+  window.confirmationResult
+    .confirm(authNumber)
+    .then((result) => {
+      // User signed in successfully.
+      const user = result.user;
+      console.log("hey YOU ARE IN SUCCESS");
+      console.log(user);
+      // ...
+    })
+    .catch((error) => {
+      // User couldn't sign in (bad verification code?)
+      // ...
+    });
+}
