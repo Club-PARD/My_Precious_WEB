3b7b6bcdf60b5bbc9e4fe7713ed6fa96c0aaaf71
LYH513 <127119407+LYH513@users.noreply.github.com>
2023-12-26 13:18:35 +0900

    Assets/img에 이미지 업로드&웹로그인 완료 페이지 추가 (#19)

--- my_precious_project/src/Components/Web/Web_Login_Components/Weblogin_1.js
+++ my_precious_project/src/Components/Web/Web_Login_Components/Weblogin_1.js
@@ -37,15 +37,15 @@ const ExplainDiv = styled.div`
   font-weight: 600;
   line-height: 51px;
 
-  width: 400px;
-  height: 102px;
-  margin-top: 45.26px;
+    //width: 400px;
+    height: 102px;
+    margin-top: 45.26px;
 `;
 
-const InputDiv = styled.div`
-  display: flex;
-  flex-direction: row;
-  margin-top: 116.87px;
+const InputDiv = styled.form`
+    display: flex;
+    flex-direction: row;
+    margin-top: 116.87px;
 `;
 
 const InputNameDiv = styled.div`
@@ -59,9 +59,12 @@ const InputNameDiv = styled.div`
 `;
 
 const GrayText = styled.div`
-  color: #d9d9d9;
-  font-size: 20px;
-  margin-bottom: 26.77px;
+    color: #6B6A6A;;
+    font-size: 20px;
+    margin-bottom: 26.77px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: 31.074px;
 `;
 
 const InputName = styled.input`
@@ -75,16 +78,19 @@ const InputName = styled.input`
   font-size: 20px;
   text-align: center;
 
-  &::placeholder {
-    color: #d9d9d9; // 플레이스홀더의 색상을 변경
-    font-size: 20px; // 플레이스홀더의 폰트 크기를 변경
-    text-align: center; // 플레이스홀더의 텍스트 정렬을 변경
-  }
+    &::placeholder {
+        color: #6B6A6A;; // 플레이스홀더의 색상을 변경
+        font-size: 20px; // 플레이스홀더의 폰트 크기를 변경
+        text-align: center; // 플레이스홀더의 텍스트 정렬을 변경
+        font-style: normal;
+        font-weight: 600;
+        line-height: 31.074px; 
+    }
 
-  &:focus {
-    border-color: #00ff00;
-    outline: none;
-  }
+    /* &:focus {
+        border-color: #00ff00;
+        outline: none;
+    } */
 `;
 
 const InputBirthDiv = styled.div`
@@ -115,23 +121,27 @@ const InputYear = styled.input`
   text-align: center;
 
   &::placeholder {
-    color: #d9d9d9;
+    color: #6B6A6A;
     font-size: 20px;
     text-align: right;
     margin-right: 11px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: 31.074px; 
   }
-  &:focus {
+  /* &:focus {
     border-color: #00ff00;
     outline: none;
-  }
+  } */
 
-  //스피너를 감춤
-  appearance: textfield; /* Firefox */
-  &::-webkit-inner-spin-button,
-  &::-webkit-outer-spin-button {
-    -webkit-appearance: none;
-    margin: 0;
-  }
+
+    //스피너를 감춤
+    appearance: textfield; /* Firefox */
+    &::-webkit-inner-spin-button,
+    &::-webkit-outer-spin-button {
+        -webkit-appearance: none;
+        margin: 0;
+    }
 `;
 
 const InputMonth = styled.input`
@@ -146,23 +156,26 @@ const InputMonth = styled.input`
   font-size: 20px;
   text-align: center;
 
-  &::placeholder {
-    color: #d9d9d9;
-    font-size: 20px;
-    text-align: right;
-    margin-right: 11px;
-  }
-  &:focus {
-    border-color: #00ff00;
-    outline: none;
-  }
+    &::placeholder {
+        color: #6B6A6A;
+        font-size: 20px;
+        text-align: right;
+        margin-right: 11px;
+        font-style: normal;
+        font-weight: 600;
+        line-height: 31.074px; 
+    }
+    /* &:focus {
+        border-color: #00ff00;
+        outline: none;
+    } */
 
-  appearance: textfield; /* Firefox */
-  &::-webkit-inner-spin-button,
-  &::-webkit-outer-spin-button {
-    -webkit-appearance: none;
-    margin: 0;
-  }
+    appearance: textfield; /* Firefox */
+    &::-webkit-inner-spin-button,
+    &::-webkit-outer-spin-button {
+        -webkit-appearance: none;
+        margin: 0;
+    }
 `;
 
 const InputDate = styled.input`
@@ -177,71 +190,80 @@ const InputDate = styled.input`
   font-size: 20px;
   text-align: center;
 
-  &::placeholder {
-    color: #d9d9d9;
-    font-size: 20px;
-    text-align: right;
-    margin-right: 11px;
-  }
-  &:focus {
-    border-color: #00ff00;
-    outline: none;
-  }
+    &::placeholder {
+        color: #6B6A6A;
+        font-size: 20px;
+        text-align: right;
+        margin-right: 11px;
+        font-style: normal;
+        font-weight: 600;
+        line-height: 31.074px; 
+    }
+    /* &:focus {
+        border-color: #00ff00;
+        outline: none;
+    } */
 
-  appearance: textfield; /* Firefox */
-  &::-webkit-inner-spin-button,
-  &::-webkit-outer-spin-button {
-    -webkit-appearance: none;
-    margin: 0;
-  }
+    appearance: textfield; /* Firefox */
+    &::-webkit-inner-spin-button,
+    &::-webkit-outer-spin-button {
+        -webkit-appearance: none;
+        margin: 0;
+    }
+`;
+
+const CheckBtn =styled.button`
+    width: 63.499px;
+    height: 55.393px;
+    flex-shrink: 0;
+    border-radius: 6.755px;
+    background: #FF3D00;
+    border: #FF3D00;
+
+    color: #F5F5F5;
+
+    text-align: center;
+    //font-family: Pretendard;
+    font-size: 20px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 31.074px;
+    cursor: pointer;
+
 `;
 
 const WebLogin_1 = () => {
   const theme = useTheme();
 
-  return (
-    <ThemeProvider theme={theme}>
-      <Container>
-        <LayoutDiv>
-          <ContentDiv>
-            <DotButton dotColor={1} />
-            <ExplainDiv>
-              고객님의 소중한 거래를 위해<br></br> 추가 정보를 기입해주세요.
-            </ExplainDiv>
-            <InputDiv>
-              <InputNameDiv>
-                <GrayText>이름</GrayText>
-                <InputName type="text" placeholder="홍길동"></InputName>
-              </InputNameDiv>
-              <InputBirthDiv>
-                <GrayText>생년월일</GrayText>
-                <InputBirthSpace>
-                  <InputYear
-                    type="number"
-                    placeholder="년"
-                    min="1800"
-                    max="2023"
-                  ></InputYear>
-                  <InputMonth
-                    placeholder="월"
-                    type="number"
-                    min="1"
-                    max="12"
-                  ></InputMonth>
-                  <InputDate
-                    placeholder="일"
-                    type="number"
-                    min="1"
-                    max="31"
-                  ></InputDate>
-                </InputBirthSpace>
-              </InputBirthDiv>
-            </InputDiv>
-          </ContentDiv>
-        </LayoutDiv>
-      </Container>
-    </ThemeProvider>
-  );
+    return (
+        <ThemeProvider theme={theme}>
+            <Container>
+                <LayoutDiv>
+                    <ContentDiv>
+                        <DotButton dotColor={1} />
+                        <ExplainDiv>
+                            고객님의 소중한 거래를 위해<br></br> 추가 정보를 기입해주세요.
+                        </ExplainDiv>
+                        <InputDiv>
+                            <InputNameDiv>
+                                <GrayText>이름</GrayText>
+                                <InputName type="text" placeholder="홍길동"></InputName>
+                            </InputNameDiv>
+                            <InputBirthDiv>
+                                <GrayText>생년월일</GrayText>
+                                <InputBirthSpace>
+                                    <InputYear type="number" placeholder="년" min="1800" max="2023"></InputYear>
+                                    <InputMonth placeholder="월" type="number" min="1" max="12"></InputMonth>
+                                    <InputDate placeholder="일" type="number" min="1" max="31"></InputDate>
+                                    <CheckBtn type='submit'>확인</CheckBtn>
+                                </InputBirthSpace>
+                            </InputBirthDiv>
+                        </InputDiv>
+                    </ContentDiv>
+                </LayoutDiv>
+            </Container>
+        </ThemeProvider>
+    );
 };
 
 export default WebLogin_1;
--- my_precious_project/src/Components/Web/Web_Login_Components/Weblogin_3.js
+++ my_precious_project/src/Components/Web/Web_Login_Components/Weblogin_3.js
@@ -1,94 +1,123 @@
-import React from "react";
-import styled, { ThemeProvider } from "styled-components";
-import { useTheme } from "../../../contexts/ThemeContext.js"; // Context APi 적용
-import DotButton from "./DotButton.js";
-import { Checkmark } from "react-checkmark";
+import React from 'react';
+import styled, { ThemeProvider } from 'styled-components';
+import { useTheme } from '../../../contexts/ThemeContext.js'; // Context APi 적용
+import DotButton from './DotButton.js';
+import loginImage from '../../../Assets/img/LoginImage.png';
+
+
 
 const WebLogin_3 = () => {
-  const theme = useTheme();
-  return (
+    const theme = useTheme();
+
+    return (
     <ThemeProvider theme={theme}>
       <Container>
-        <ContentBox>
-          <InnerRow1>
-            <DotButton dotColor={3} />
-          </InnerRow1>
-          <InnerRow2>휴대폰 인증이 완료되었어요!</InnerRow2>
-          <InnerRow3>
-            <Checkmark size="xLarge" color="#ff3d00"></Checkmark>
-            <Div>
-              등록하신 휴대폰 인증을 통해<br></br>전자서명을 따로 하시지 않아도
-              되는 편리함이 생겼어요.
-            </Div>
-            <form>
-              <Button>확인</Button>
-            </form>
-          </InnerRow3>
-        </ContentBox>
+          <ContentBox>
+            <InnerRow1>
+                <DotButton dotColor={3} />
+            </InnerRow1>
+            <InnerRow2>너무 환영합니다! <br/>우리 머니글러브의 45번째 머글이 되어주셔서 감사해요.</InnerRow2>
+            <InnerRow3>
+              <ImageDiv>
+                <Img src={loginImage} alt='로그인페이지 기본 이미지'></Img>
+                <NameDiv>김현지님</NameDiv>
+              </ImageDiv>
+              <form>
+                  <Button>시작하기</Button>
+              </form>
+            </InnerRow3>
+          </ContentBox>
       </Container>
     </ThemeProvider>
-  );
+    );
 };
 
 const Container = styled.div`
-  display: flex;
-  flex-direction: row;
-  margin: 0;
-  padding: 0;
-  height: 100vh;
-  justify-content: center;
+    display: flex;
+    flex-direction: row;
+    margin: 0;
+    padding: 0;
+    height: 100vh;
+    justify-content: center;
 `;
 
 const ContentBox = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: 500px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    //width: 500px;
 `;
 const InnerRow1 = styled.div`
-  display: flex;
-  justify-content: center;
-  width: 100%;
-  margin-top: 155px;
-  margin-bottom: 50px;
+    display: flex;
+    justify-content: center;
+    width: 100%;
+    margin-top: 127px;
+    margin-bottom: 50px;
 `;
 
 const InnerRow2 = styled.div`
-  color: #000;
-  text-align: center;
-  font-size: 32px;
-  font-weight: 600;
-  line-height: 50px;
-  width: 100%;
-  margin-bottom: 100px;
+    color: #0F0F0F;
+    text-align: center;
+    font-size: 32px;
+    font-weight: 600;
+    line-height: 50px;
+    width: 100%;
+    white-space: pre-wrap;
 `;
 
 const InnerRow3 = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  text-align: center;
-  width: 100%;
-  font-size: 20px;
-  font-weight: 500;
-  color: #d9d9d9;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+    width: 100%;
+    font-size: 20px;
+    font-weight: 500;
+    color: #d9d9d9;
 `;
 
-const Div = styled.div`
+const ImageDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 39px;
+  width: 250px;
+  height: 292px;
+  flex-shrink: 0;
+
+  border-radius: 20px;
+  border: 2px solid #FF3D00;
+  align-items: center;
+`;
+
+const Img =styled.img`
   margin-top: 50px;
 `;
 
+const NameDiv = styled.div`
+  width: 155px;
+  height: 44px;
+  flex-shrink: 0;
+  margin-top: 43.71px;
+  color: #FF3D00;
+  text-align: center;
+  //font-family: Pretendard;
+  font-size: 32.886px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+`;
+
 const Button = styled.button`
-  margin-top: 100px;
-  width: 450px;
-  height: 55px;
-  border-radius: 6.5px;
-  border: none;
-  color: white;
-  font-size: 20px;
-  font-weight: 500;
-  background-color: #ff3d00;
-  cursor: pointer;
+    margin-top: 49px;
+    width: 450px;
+    height: 55px;
+    border-radius: 6.5px;
+    border: none;
+    color: white;
+    font-size: 20px;
+    font-weight: 500;
+    background-color: #ff3d00;
+    cursor: pointer;
 `;
 
 export default WebLogin_3;
