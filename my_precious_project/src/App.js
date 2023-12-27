95cdfa36522abb28ab2f86ec76070ea715cd32fb
hjkim0905 <eax9952@gmail.com>
2023-12-27 12:42:18 +0900

    충돌 해결

--- my_precious_project/src/App.js
+++ my_precious_project/src/App.js
@@ -1,12 +1,12 @@
-<<<<<<< HEAD
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import AboutPage from './Page/AboutPage';
 import HomePage from './Page/HomePage';
 import ScrollToTop from './ScrollToTop';
 import { ThemeProvider } from './contexts/ThemeContext.js';
 import { theme } from './Style/theme';
-import FirebaseContainer from './Page/loginfeat.js';
 import LoginPage from './Page/LoginPage';
+import RequestPage from './Page/RequestPage.js';
+import DashboardPage from './Page/DashboardPage.js';
 
 function App() {
     return (
@@ -14,42 +14,15 @@ function App() {
             <Router>
                 <ScrollToTop />
                 <Routes>
-                    {/* 기본 화면 설정 */}
-                    <Route path="/phone" element={<FirebaseContainer />} />
                     <Route path="/login/*" element={<LoginPage />} />
                     <Route path="/" element={<HomePage />} />
+                    <Route path="/request" element={<RequestPage />} />
                     <Route path="/about" element={<AboutPage />} />
+                    <Route path="/dashboard" element={<DashboardPage />} />
                 </Routes>
             </Router>
         </ThemeProvider>
     );
-=======
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import AboutPage from "./Page/AboutPage";
-import HomePage from "./Page/HomePage";
-import ScrollToTop from "./ScrollToTop";
-import { ThemeProvider } from "./contexts/ThemeContext.js";
-import { theme } from "./Style/theme";
-import LoginPage from "./Page/LoginPage";
-import RequestPage from "./Page/RequestPage.js";
-import DashboardPage from "./Page/DashboardPage.js"
-
-function App() {
-  return (
-    <ThemeProvider theme={theme}>
-      <Router>
-        <ScrollToTop />
-        <Routes>
-          <Route path="/login/*" element={<LoginPage />} />
-          <Route path="/" element={<HomePage />} />
-          <Route path="/request" element={<RequestPage />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/dashboard" element={<DashboardPage />} />
-        </Routes>
-      </Router>
-    </ThemeProvider>
-  );
->>>>>>> main
 }
 
 export default App;
