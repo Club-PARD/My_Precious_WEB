132d2b3edc995fab140a72363cd6c6031ca62c43
minzziPark <minzzi0068@gmail.com>
2024-01-03 00:32:13 +0900

    240103 민지 : manage-request 페이지 서버 오류 수정

--- my_precious_project/src/Components/Web/Web_ManageRequest_Components/ManageSummary.js
+++ my_precious_project/src/Components/Web/Web_ManageRequest_Components/ManageSummary.js
@@ -1,280 +1,287 @@
-import React, { useState,useEffect } from 'react';
-import { useNavigate} from 'react-router-dom';
-import styled, { ThemeProvider } from 'styled-components';
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import styled, { ThemeProvider } from "styled-components";
 //import { Link } from 'react-router-dom';
-import { useTheme } from '../../../contexts/ThemeContext.js.js'; // Context APi 적용
-import SmallLineProgress from './SmallLineProgress.js';
-import Character from '../../../Assets/img/Character.png';
-import Talk from '../../../Assets/img/Talk.svg';
-import axios from 'axios';
+import { useTheme } from "../../../contexts/ThemeContext.js.js"; // Context APi 적용
+import SmallLineProgress from "./SmallLineProgress.js";
+import Character from "../../../Assets/img/Character.png";
+import Talk from "../../../Assets/img/Talk.svg";
+import axios from "axios";
 
 const Container = styled.div`
-    display: flex;
-    flex-direction: column;
-    margin: 0;
-    padding: 0;
-    width: 51.875rem;
-    height: 14.3125rem;
-    flex-shrink: 0;
-    border-radius: 0.625rem;
-    background: #FBFBFB;
-    box-shadow: 0px 1px 3px 0px rgba(0, 0, 0, 0.15);
-    position: relative;
+  display: flex;
+  flex-direction: column;
+  margin: 0;
+  padding: 0;
+  width: 51.875rem;
+  height: 14.3125rem;
+  flex-shrink: 0;
+  border-radius: 0.625rem;
+  background: #fbfbfb;
+  box-shadow: 0px 1px 3px 0px rgba(0, 0, 0, 0.15);
+  position: relative;
 `;
 
-const DisplayMoneyContainer =styled.div`
-    display: flex;
-    flex-direction: row;
+const DisplayMoneyContainer = styled.div`
+  display: flex;
+  flex-direction: row;
 `;
 
-const DisplayMoneyDiv =styled.div`
-    display: flex;
-    flex-direction: column;
-    width: 23.46875rem;
-    padding-top: 1.44rem;
+const DisplayMoneyDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 23.46875rem;
+  padding-top: 1.44rem;
 `;
 
-const MoneyText =styled.div`
-    display: flex;
-    color: #3E3E3E;
-    font-family: Pretendard;
-    font-size: 1.5rem;
-    font-style: normal;
-    font-weight: 700;
-    line-height: normal;
-    padding-bottom: 0.74rem;
+const MoneyText = styled.div`
+  display: flex;
+  color: #3e3e3e;
+  font-family: Pretendard;
+  font-size: 1.5rem;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  padding-bottom: 0.74rem;
 `;
 
-const Row =styled.div`
-    display: flex;
-    flex-direction: row;
-    color: #5B5B5B;
-    align-items: center;
+const Row = styled.div`
+  display: flex;
+  flex-direction: row;
+  color: #5b5b5b;
+  align-items: center;
 
-    text-align: right;
-    font-family: Pretendard;
-    font-size: 0.75rem;
-    font-style: normal;
-    font-weight: 600;
-    line-height: normal;
-    padding-top: 0.36rem;
+  text-align: right;
+  font-family: Pretendard;
+  font-size: 0.75rem;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+  padding-top: 0.36rem;
 `;
 
-const Circle =styled.div`
-    width: 0.4375rem;
-    height: 0.4375rem;
-    flex-shrink: 0;
-    border: none;
-    background-color: #FC511C;
-    border-radius: 50%; 
-    margin-right: 0.25rem;
+const Circle = styled.div`
+  width: 0.4375rem;
+  height: 0.4375rem;
+  flex-shrink: 0;
+  border: none;
+  background-color: #fc511c;
+  border-radius: 50%;
+  margin-right: 0.25rem;
 `;
 
-const RightRowDiv =styled.div`
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    color: #5B5B5B;
-    text-align: right;
-    font-family: Pretendard;
-    font-size: 0.75rem;
-    font-style: normal;
-    font-weight: 600;
-    line-height: normal;
-    align-items: center;
+const RightRowDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  color: #5b5b5b;
+  text-align: right;
+  font-family: Pretendard;
+  font-size: 0.75rem;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+  align-items: center;
 `;
 
-const UnderGrayDiv =styled.div`
-    display: flex;
-    flex-direction: column;
-    padding-top:2.3rem;
-    padding-left: 1.56rem;
-    gap: 0.37rem;
-    justify-content: center;
+const UnderGrayDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding-top: 2.3rem;
+  padding-left: 1.56rem;
+  gap: 0.37rem;
+  justify-content: center;
 `;
 
-const UnderGrayFirstDiv =styled.div`
-    display: flex;
-    flex-direction: row;
-    color: #696969;
-    font-family: Pretendard;
-    font-size: 0.875rem;
-    font-style: normal;
-    font-weight: 600;
-    line-height: normal;
-
+const UnderGrayFirstDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  color: #696969;
+  font-family: Pretendard;
+  font-size: 0.875rem;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
 `;
 
-const Dday =styled.div`
-    display: flex;
-    width: 5.1875rem;
-    height: 1.375rem;
-    flex-shrink: 0;
-    border-radius: 0.4375rem;
-    border: 1px solid #FF3D00;
-    color: #FF3D00;
-    text-align: center;
-    font-family: Pretendard;
-    font-size: 0.875rem;
-    font-style: normal;
-    font-weight: 600;
-    line-height: normal;
-    align-items: center;
-    justify-content: center;
-    text-align: center;
-    margin-left: 1.12rem;
+const Dday = styled.div`
+  display: flex;
+  width: 5.1875rem;
+  height: 1.375rem;
+  flex-shrink: 0;
+  border-radius: 0.4375rem;
+  border: 1px solid #ff3d00;
+  color: #ff3d00;
+  text-align: center;
+  font-family: Pretendard;
+  font-size: 0.875rem;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  margin-left: 1.12rem;
 `;
 
 const ImageCharacter = styled.div`
-    position: absolute;
-    width: 5rem;
-    height: 5rem;
-    background-image:url(${Character});
-    background-repeat:no-repeat;
-    background-size: contain;
-    top: 3%;
-    left: 80%;
-    z-index: 1;
-    display: flex;
+  position: absolute;
+  width: 5rem;
+  height: 5rem;
+  background-image: url(${Character});
+  background-repeat: no-repeat;
+  background-size: contain;
+  top: 3%;
+  left: 80%;
+  z-index: 1;
+  display: flex;
 `;
 
 const ImageTalk = styled.div`
-    position: absolute;
-    background-image:url(${Talk});
-    background-repeat:no-repeat;
-    background-size: contain;
-    top: 0%;
-    left: 85%;
-    z-index: 1;
-    display: flex;
-    width: 11rem;
-    height: 3.9375rem;
-    flex-shrink: 0;
-    //justify-content: center;
+  position: absolute;
+  background-image: url(${Talk});
+  background-repeat: no-repeat;
+  background-size: contain;
+  top: 0%;
+  left: 85%;
+  z-index: 1;
+  display: flex;
+  width: 11rem;
+  height: 3.9375rem;
+  flex-shrink: 0;
+  //justify-content: center;
 `;
 
-const TalkText =styled.div`
-    display: flex;
-    color: #5B5B5B;
-    font-family: Pretendard;
-    font-size: 0.875rem;
-    font-style: normal;
-    font-weight: 600;
-    line-height: normal;
-    width: 8.6rem;
-    height: 2.2rem;
-    padding-top: 0.5rem;
-    padding-left: 1rem;
+const TalkText = styled.div`
+  display: flex;
+  color: #5b5b5b;
+  font-family: Pretendard;
+  font-size: 0.875rem;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+  width: 8.6rem;
+  height: 2.2rem;
+  padding-top: 0.5rem;
+  padding-left: 1rem;
 `;
 
-function ManageSummary({manageData,boardId}) {
-    const theme = useTheme();
+function ManageSummary({ manageData, boardId }) {
+  const theme = useTheme();
 
-    const [detailData, setDetailData] = useState({
-        total: 10000, //이만큼 돈 주세요
-        receive: 5000, //이만큼 모였어요
-        title: "",
-        reason: "",
-        plan: "",
-        date: "",
-        bank: "",
-        account:"",
-        name:"" ,
-        lendMoneyCount: 0 , // 빌려준 친구의 수
-        ConfirmCount: 0, //갚은 사람 수
-        totalConfirmMoney: 0, // 갚은 돈 총액
-    });
+  const [BorrowDataSet, setBorrowDataSet] = useState([]);
 
-    useEffect(() => {
-        axios
-          .get(`http://moneyglove-env.eba-xt43tq6x.ap-northeast-2.elasticbeanstalk.com/api/v9/debts/confirmedDebts/${boardId}`)
-          .then((response) => {
+  const [detailData, setDetailData] = useState({
+    total: 10000, //이만큼 돈 주세요
+    receive: 5000, //이만큼 모였어요
+    title: "",
+    reason: "",
+    plan: "",
+    date: "",
+    bank: "",
+    account: "",
+    name: "",
+    lendMoneyCount: 0, // 빌려준 친구의 수
+    ConfirmCount: 0, //갚은 사람 수
+    totalConfirmMoney: 0, // 갚은 돈 총액
+  });
 
+  const getData = async () => {
+    try {
+      const response = await axios.get(
+        `http://moneyglove-env.eba-xt43tq6x.ap-northeast-2.elasticbeanstalk.com/api/v9/debts/confirmedDebts/${boardId}`
+      );
+      const borrowData = response.data.data;
+      if (borrowData) {
+        const transformedBorrowData = borrowData.map((item) => {
+          return {
+            id: item.id,
+            lendMoney: item.lendMoney,
+            repaymentStatus: item.repaymentStatus,
+          };
+        });
+        setBorrowDataSet(transformedBorrowData);
+        console.log(transformedBorrowData);
+      }
+    } catch (error) {
+      console.error("API에서 데이터를 가져오는데 실패했습니다:", error);
+    }
+  };
 
-            const num =response.data.data
-            //갚은 사람의 수
-            const ConfirmCount =parseFloat(num.length);
+  useEffect(() => {
+    getData();
+  }, []);
 
-            // 갚은 돈 더하기
-            const mapConfirm =num.map((value, index) =>{
-                 return parseFloat(value.lendMoney);
-            });
-            const totalConfirmMoney = mapConfirm.reduce((accumulator, currentValue) => {
-                 return accumulator + currentValue;
-             }, 0);
-
-            console.log(totalConfirmMoney)
-
-            setDetailData({
-                ...detailData,
-                ConfirmCount: ConfirmCount,
-                totalConfirmMoney: totalConfirmMoney
-            });
-
-          })
-          .catch((error) => {
-            console.error("데이터 전송 중 오류 발생: ", error);
-            // 오류를 처리합니다.
-          });
-    }, [detailData.ConfirmCount,detailData.totalConfirmMoney]);
-
-    //받은 돈 숫자에서 문자 -> 컴마 추가
-    var receiveNumber = manageData.totalLendmoney;
+  //받은 돈 숫자에서 문자 -> 컴마 추가
+  var receiveNumber = manageData.totalLendmoney;
+  if (receiveNumber !== undefined) {
     var LendformattedNumber = receiveNumber.toLocaleString();
-    //필요한 돈 숫자에서 문자 -> 컴마 추가
-    var totaleNumber = manageData.borrowMoney;
+  }
+
+  //필요한 돈 숫자에서 문자 -> 컴마 추가
+  var totaleNumber = manageData.borrowMoney;
+  if (totaleNumber !== undefined) {
     var NeedformattedNumber = totaleNumber.toLocaleString();
+  }
 
-    var ConfirmNumber =detailData.totalConfirmMoney;
-    var ConfirmformattedNumber =ConfirmNumber.toLocaleString();
+  var ConfirmNumber = detailData.totalConfirmMoney;
+  if (ConfirmNumber !== undefined) {
+    var ConfirmformattedNumber = ConfirmNumber.toLocaleString();
+  }
 
+  return (
+    <ThemeProvider theme={theme}>
+      <Container>
+        <ImageCharacter />
+        <ImageTalk>
+          <TalkText>
+            현재까지 {detailData.ConfirmCount}명의 친구에게 돈을 갚았어요.
+          </TalkText>
+        </ImageTalk>
+        <DisplayMoneyContainer>
+          <DisplayMoneyDiv style={{ paddingLeft: "1.56rem" }}>
+            <MoneyText>현재까지 모인 금액</MoneyText>
+            <SmallLineProgress
+              total={parseFloat(manageData.borrowMoney)}
+              receive={parseFloat(manageData.totalLendmoney)}
+            />
+            <Row>
+              <Circle /> 모인 금액 {LendformattedNumber}
+            </Row>
+          </DisplayMoneyDiv>
+          <DisplayMoneyDiv style={{ paddingLeft: "1.25rem" }}>
+            <MoneyText>갚은 금액</MoneyText>
+            <SmallLineProgress
+              total={parseFloat(manageData.totalLendmoney)}
+              receive={detailData.totalConfirmMoney}
+            />
+            <RightRowDiv>
+              <Row>
+                <Circle />
+                갚은 금액 {ConfirmformattedNumber}
+              </Row>
+            </RightRowDiv>
+          </DisplayMoneyDiv>
+        </DisplayMoneyContainer>
+        <UnderGrayDiv>
+          <UnderGrayFirstDiv>
+            <div style={{ paddingRight: "6.19rem" }}>필요 금액</div>
+            <div>{NeedformattedNumber} 원</div>
+          </UnderGrayFirstDiv>
+          <UnderGrayFirstDiv>
+            <div style={{ paddingRight: "2.3rem" }}>갚기로 한 약속날짜</div>
+            <div>{manageData.formatted_date}</div>
 
-    return (
-        <ThemeProvider theme={theme}>
-            <Container>
-                <ImageCharacter/>
-                <ImageTalk>
-                    <TalkText>현재까지 {detailData.ConfirmCount}명의 친구에게
-                    돈을 갚았어요.
-                    </TalkText>
-                </ImageTalk>
-                <DisplayMoneyContainer>
-                <DisplayMoneyDiv style={{paddingLeft:"1.56rem"}}>
-                    <MoneyText>현재까지 모인 금액</MoneyText>
-                    <SmallLineProgress total={parseFloat(manageData.borrowMoney)} receive={parseFloat(manageData.totalLendmoney)} />
-                    <Row>
-                        <Circle/> 모인 금액 {LendformattedNumber}
-                    </Row>
-                </DisplayMoneyDiv>
-                <DisplayMoneyDiv style={{paddingLeft:"1.25rem"}}>
-                    <MoneyText>갚은 금액</MoneyText>
-                    <SmallLineProgress total={parseFloat(manageData.totalLendmoney)} receive={detailData.totalConfirmMoney} />
-                    <RightRowDiv>
-                        <Row>
-                            <Circle/>갚은 금액 {ConfirmformattedNumber}
-                        </Row>
-                    </RightRowDiv>
-                </DisplayMoneyDiv>
-                </DisplayMoneyContainer>
-                <UnderGrayDiv>
-                    <UnderGrayFirstDiv>
-                        <div style={{paddingRight:"6.19rem"}}>필요 금액</div>
-                        <div>{NeedformattedNumber} 원</div>
-                    </UnderGrayFirstDiv>
-                    <UnderGrayFirstDiv>
-                        <div style={{paddingRight:"2.3rem"}}>갚기로 한 약속날짜</div>
-                        <div>{manageData.formatted_date}</div>
-                        
-                        {manageData.dday >=0 ? (
-                        <Dday>D-{manageData.dday}</Dday>
-                        ) :(
-                            <Dday>D+{Math.abs(manageData.dday)} </Dday>
-                        )}
-                    </UnderGrayFirstDiv>
-                </UnderGrayDiv>
-            </Container>
-        </ThemeProvider>
-    );
+            {manageData.dday >= 0 ? (
+              <Dday>D-{manageData.dday}</Dday>
+            ) : (
+              <Dday>D+{Math.abs(manageData.dday)} </Dday>
+            )}
+          </UnderGrayFirstDiv>
+        </UnderGrayDiv>
+      </Container>
+    </ThemeProvider>
+  );
 }
 
 export default ManageSummary;
