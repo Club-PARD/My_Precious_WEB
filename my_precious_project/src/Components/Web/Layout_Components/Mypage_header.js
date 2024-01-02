4dd10bd64d15b1abb0e306bd722eb06a2a34e719
minzziPark <minzzi0068@gmail.com>
2024-01-02 16:55:26 +0900

    240102 민지 : 해더 상단 고정

--- my_precious_project/src/Components/Web/Layout_Components/Mypage_header.js
+++ my_precious_project/src/Components/Web/Layout_Components/Mypage_header.js
@@ -1,86 +1,89 @@
-import React from 'react';
-import styled, { ThemeProvider } from 'styled-components';
+import React from "react";
+import styled, { ThemeProvider } from "styled-components";
 //import { Link } from 'react-router-dom';
-import { useTheme } from '../../../contexts/ThemeContext.js'; // Context APi 적용
-import MGLogo from '../../../Assets/img/MGLogo.svg';
+import { useTheme } from "../../../contexts/ThemeContext.js"; // Context APi 적용
+import MGLogo from "../../../Assets/img/MGLogo.svg";
 
 const Header = (props) => {
-    const theme = useTheme();
+  const theme = useTheme();
 
-    return (
-        <ThemeProvider theme={theme}>
-            <Navigation bgcolor={props.color}>
-                <LogoImg src={MGLogo} alt="로고이미지"></LogoImg>
-                <NavRightSideDiv>
-                    <AboutBtn>ABOUT US</AboutBtn>
-                    <DashboardBtn>DASHBOARD</DashboardBtn>
-                </NavRightSideDiv>
-            </Navigation>
-        </ThemeProvider>
-    );
+  return (
+    <ThemeProvider theme={theme}>
+      <Navigation bgcolor={props.color}>
+        <LogoImg src={MGLogo} alt="로고이미지"></LogoImg>
+        <NavRightSideDiv>
+          <AboutBtn>ABOUT US</AboutBtn>
+          <DashboardBtn>DASHBOARD</DashboardBtn>
+        </NavRightSideDiv>
+      </Navigation>
+    </ThemeProvider>
+  );
 };
 
 const Navigation = styled.div`
-    display: flex;
-    flex-direction: row;
-    width: 80%;
-    padding-top: 33px;
-    padding-bottom: 75px;
-    flex-shrink: 0;
-    background: ${(props) => (props.bgcolor === 1 ? '#E5E5E5' : '#FAFAFA')};
-    justify-content: space-between;
-    align-items: start;
+  display: flex;
+  flex-direction: row;
+  width: 80%;
+  /* padding-top: 33px;
+  padding-bottom: 75px; */
+  flex-shrink: 0;
+  background: ${(props) => (props.bgcolor === 1 ? "#E5E5E5" : "#FAFAFA")};
+  justify-content: space-between;
+  align-items: center;
+  position: fixed;
+  /* left: 0; */
+  top: 1.94rem;
 
-    & button {
-        background: ${(props) => (props.bgcolor === 1 ? '#E5E5E5' : '#FAFAFA')};
-    }
+  & button {
+    background: ${(props) => (props.bgcolor === 1 ? "#E5E5E5" : "#FAFAFA")};
+  }
 
-    & div {
-        background: ${(props) => (props.bgcolor === 1 ? '#E5E5E5' : '#FAFAFA')};
-    }
+  & div {
+    background: ${(props) => (props.bgcolor === 1 ? "#E5E5E5" : "#FAFAFA")};
+  }
 `;
 
 const LogoImg = styled.img`
-    width: 135px;
-    height: 46.5px;
+  width: 135px;
+  height: 46.5px;
 `;
 
 const NavRightSideDiv = styled.div`
-    display: flex;
-    border: none;
-    width: 12.75rem;
+  display: flex;
+  border: none;
+  width: 12.75rem;
 `;
 const AboutBtn = styled.button`
-    display: flex;
-    border: none;
-    padding: 0;
-    margin: 0;
+  display: flex;
+  border: none;
+  padding: 0;
+  margin: 0;
 
-    color: #504f4f;
+  color: #504f4f;
 
-    font-family: Work Sans;
-    font-size: 0.875rem;
-    font-style: normal;
-    font-weight: 600;
-    line-height: normal;
-    cursor: pointer;
+  font-family: Work Sans;
+  font-size: 0.875rem;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+  cursor: pointer;
 `;
 
 const DashboardBtn = styled.button`
-    display: flex;
-    border: none;
-    padding: 0;
-    margin: 0;
-    margin-left: 2.5625rem;
+  display: flex;
+  border: none;
+  padding: 0;
+  margin: 0;
+  margin-left: 2.5625rem;
 
-    color: #ff3d00;
+  color: #ff3d00;
 
-    font-family: Work Sans;
-    font-size: 0.875rem;
-    font-style: normal;
-    font-weight: 700;
-    line-height: normal;
-    cursor: pointer;
+  font-family: Work Sans;
+  font-size: 0.875rem;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  cursor: pointer;
 `;
 
 export default Header;
